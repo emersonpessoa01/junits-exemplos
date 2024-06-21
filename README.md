20fe33151f16c88cc693ee622e6a2217e837b58e
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-06-21 16:14:32 -0300

    updated README :rocket:

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# junits-exemplos
-### Para gerar um projeto Maven 
 
+# Testes unitários com JUnit
+
+Respositório com roteiros e exemplos de como utilizar JUnit no seu dia a dia como dev.  
+Foi utilizado para os exemplos a versão 5.8.2, mais atual até o momento (29/01/2022).
+
+## Roteiros
+
+1. [Configurando JUnit](CONFIGURACAO.md)
+
+### Para gerar um projeto Maven
 
 ```
 mvn archetype:generate -DartifactId=nome-do-projeto -DgroupId=br.com.nome-do-pacote -DinteractiveMode=false -DarchetypeArtifactId=maven-archetype-quickstart
--- src/test/java/one/digitalinnovation/junit/AssertionsTeste.java
+++ src/test/java/one/digitalinnovation/junit/AssertionsTeste.java
@@ -7,32 +7,55 @@ import java.time.LocalDateTime;
 
 import static org.junit.Assert.*;
 
-public class assertionsTeste {
+public class AssertionsTeste {
     @Test
-    public void validarLancamentos(){
-        int[] primeiroLancamento = {10,20,30,40,50};
-        int[] segundoLancamento = {10,20,30,40,50};
-
-
+    public void validarLancamentos() {
+        /* Valida se os arrays são iguais, comparando um a um */
+        int[] primeiroLancamento = {10, 20, 30, 40, 50};
+        int[] segundoLancamento = {10, 20, 30, 40, 50};
         assertArrayEquals(primeiroLancamento, segundoLancamento);
-        
+
+        /* Utilizando arrays de diferentes tipos, começando por boolean[] */
+        boolean[] flags = {true, false, false};
+        assertArrayEquals(new boolean[]{true, false, false}, flags);
+
     }
+
     @Test
-    public void validaSeObjetoEstaNulo(){
+    public void validaSeObjetoEstaNulo() {
         Pessoa pessoa = null;
         assertNull(pessoa);
 
-       pessoa = new Pessoa("Luciano", LocalDateTime.now());
-       assertNotNull(pessoa);
+        pessoa = new Pessoa("Luciano", LocalDateTime.now());
+        assertNotNull(pessoa);
 
     }
+
     @Test
-    public void validaNumerousDifferentiates(){
+    public void validaNomesDiferentes() {
         String nome = "Emerson";
         String sobreNome = "Pessoa";
 
         assertNotNull(nome, sobreNome);
     }
 
+    @Test
+    public void validaValoresDiferentes() {
+        int x = 5;
+        int y = 5;
 
+        assertNotEquals(5, 6);
+    }
+
+    @Test
+    public void validaAlgumaCondicaoEhFalse(){
+        boolean condicao = 5 + 6 == 12;
+        assertFalse(condicao);
+    }
+
+    @Test
+    public  void validaAlgumaCondicaoEhVerdadeiro(){
+        boolean condicao = 6 + 6 == 12;
+        assertTrue(condicao);
+    }
 }
